3aa71f5fc9e0b25b12e48e24d9d3f36d6adcb6c2
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-01-25 15:49:05 +0000

    update import

--- components/infra/import.tf
+++ components/infra/import.tf
@@ -3,25 +3,25 @@ data "azurerm_client_config" "current" {
 
 import {
   to = azurerm_resource_group.this
-  id = "/subscriptions/${azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.this.name}"
+  id = "/subscriptions/${azurerm_client_config.current.subscription_id}/resourceGroups/alerts-slack-${var.env}"
 }
 
 import {
   to = azurerm_service_plan.this
-  id = "/subscriptions/${azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.this.name}/providers/Microsoft.Web/serverFarms/${azurerm_resource_group.this.name}"
+  id = "/subscriptions/${azurerm_client_config.current.subscription_id}/resourceGroups/alerts-slack-${var.env}/providers/Microsoft.Web/serverFarms/alerts-slack-${var.env}"
 }
 
 import {
   to = azurerm_storage_account.this
-  id = "/subscriptions/${azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.this.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.this.name}"
+  id = "/subscriptions/${azurerm_client_config.current.subscription_id}/resourceGroups/alerts-slack-${var.env}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.this.name}"
 }
 
 import {
   to = azurerm_windows_function_app.this
-  id = "/subscriptions/${azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.this.name}/providers/Microsoft.Web/sites/${azurerm_resource_group.this.name}"
+  id = "/subscriptions/${azurerm_client_config.current.subscription_id}/resourceGroups/alerts-slack-${var.env}/providers/Microsoft.Web/sites/alerts-slack-${var.env}"
 }
 
 import {
   to = module.application_insights.azurerm_application_insights.this
-  id = "/subscriptions/${azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.this.name}/providers/microsoft.insights/components/${module.application_insights.azurerm_application_insights.this.name}"
+  id = "/subscriptions/${azurerm_client_config.current.subscription_id}/resourceGroups/alerts-slack-${var.env}/providers/microsoft.insights/components/${module.application_insights.azurerm_application_insights.this.name}"
 }
