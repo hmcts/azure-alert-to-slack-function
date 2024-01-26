ace4381e1a7693028ec7b22630cc68806a1835ac
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-01-26 14:40:22 +0000

    revert to stg and add local

--- components/infra/app-insights.tf
+++ components/infra/app-insights.tf
@@ -2,7 +2,7 @@ module "application_insights" {
   source = "git::https://github.com/hmcts/terraform-module-application-insights?ref=main"
 
   product = var.product
-  env     = var.env
+  env     = local.env
 
   resource_group_name = azurerm_resource_group.this.name
 
