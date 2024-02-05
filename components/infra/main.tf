735f3bc0a33c73c95cb37a7f74611901678e1a9f
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-02-05 12:40:32 +0000

    update env

--- components/infra/main.tf
+++ components/infra/main.tf
@@ -24,7 +24,7 @@ module "tags" {
 
 resource "azurerm_user_assigned_identity" "this" {
   location            = azurerm_resource_group.this.location
-  name                = "${var.product}-${var.component}-${var.env}"
+  name                = "${var.product}-${var.component}-${local.env}"
   resource_group_name = azurerm_resource_group.this.name
 }
 
