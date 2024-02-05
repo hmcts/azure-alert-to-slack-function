1879017e936baab56b6891b412b21d084ccaa8c4
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-02-05 11:33:33 +0000

    update envs and pipeline

--- components/infra/app-insights.tf
+++ components/infra/app-insights.tf
@@ -2,7 +2,7 @@ module "application_insights" {
   source = "git::https://github.com/hmcts/terraform-module-application-insights?ref=main"
 
   product = var.product
-  env     = local.env
+  env     = var.env
 
   resource_group_name = azurerm_resource_group.this.name
 
--- components/infra/main.tf
+++ components/infra/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "this" {
-  name     = "${var.product}-${var.component}-${local.env}"
+  name     = "${var.product}-${var.component}-${var.env}"
   location = var.location
 
   tags = module.tags.common_tags
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = replace("${var.product}${var.component}${local.env}", "-", "")
+  name                     = replace("${var.product}${var.component}${var.env}", "-", "")
   resource_group_name      = azurerm_resource_group.this.name
   location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
@@ -17,14 +17,14 @@ resource "azurerm_storage_account" "this" {
 
 module "tags" {
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment = local.env
+  environment = var.env
   product     = "cft-platform"
   builtFrom   = var.builtFrom
 }
 
 resource "azurerm_user_assigned_identity" "this" {
   location            = azurerm_resource_group.this.location
-  name                = "${var.product}-${var.component}-${local.env}"
+  name                = "${var.product}-${var.component}-${var.env}"
   resource_group_name = azurerm_resource_group.this.name
 }
 
