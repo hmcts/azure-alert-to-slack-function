a2aaec88a2f57108e9f24a6b42e9f61cc05ca320
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-02-05 12:33:19 +0000

    fix logic

--- components/infra/local.tf
+++ components/infra/local.tf
@@ -1,3 +1,3 @@
 locals {
-  env = var.env == "stg" && strcontains(lower(data.azurerm_subscription.current.display_name), "cftapps") ? "aat" : var.env || var.env == "test" && strcontains(lower(data.azurerm_subscription.current.display_name), "cftapps") ? "perftest" : var.env
+  env = (var.env == "stg" && strcontains(lower(data.azurerm_subscription.current.display_name), "cftapps")) ? "aat" : (var.env == "test" && strcontains(lower(data.azurerm_subscription.current.display_name), "cftapps")) ? "perftest" : var.env
 }
