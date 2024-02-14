8d6949201c9423e2e5bf534c3c707f5500860faf
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-02-14 11:27:07 +0000

    temp patch

--- components/infra/app-insights.tf
+++ components/infra/app-insights.tf
@@ -1,5 +1,5 @@
 module "application_insights" {
-  source = "git::https://github.com/hmcts/terraform-module-application-insights?ref=main"
+  source = "git::https://github.com/hmcts/terraform-module-application-insights?ref=chirag-patch-1"
 
   product = "${local.business_area}-${var.product}"
   env     = local.env
