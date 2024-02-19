7bd099c28ced56006cfffee1520b6f4a2fd6f433
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-02-19 11:17:53 +0000

    switch brnach name back to main

--- components/infra/app-insights.tf
+++ components/infra/app-insights.tf
@@ -1,5 +1,5 @@
 module "application_insights" {
-  source = "git::https://github.com/hmcts/terraform-module-application-insights?ref=DTSPO-16829-prepare"
+  source = "git::https://github.com/hmcts/terraform-module-application-insights?ref=main"
 
   product = "${local.business_area}-${var.product}"
   env     = local.env
