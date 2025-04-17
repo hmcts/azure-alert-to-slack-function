3fcc52021c01796d1fd3fb8defd867547a57cb1b
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-04-17 13:25:56 +0100

    add condition

--- components/infra/data.tf
+++ components/infra/data.tf
@@ -22,6 +22,6 @@ data "azurerm_key_vault_secret" "signing_secret" {
 
 data "azuread_application" "bootstrap" {
   for_each     = local.business_area == "cft" ? toset(local.cft_subscriptions) : toset(local.sds_subscriptions)
-  display_name = "DTS Bootstrap (sub:${each.value})"
+  display_name = each.value == "DTS-HERITAGE-EXTSVC-PROD" ? null : "DTS Bootstrap (sub:${each.value})"
   client_id    = each.value == "DTS-HERITAGE-EXTSVC-PROD" ? "5f29910d-50d4-424b-a888-e95c59dc9d70" : null
 }
