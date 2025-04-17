3917a97fe028f9880bf10ba429287369cd44dd86
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-04-17 13:30:40 +0100

    fix id

--- components/infra/data.tf
+++ components/infra/data.tf
@@ -23,5 +23,5 @@ data "azurerm_key_vault_secret" "signing_secret" {
 data "azuread_application" "bootstrap" {
   for_each     = local.business_area == "cft" ? toset(local.cft_subscriptions) : toset(local.sds_subscriptions)
   display_name = each.value == "DTS-HERITAGE-EXTSVC-PROD" ? null : "DTS Bootstrap (sub:${each.value})"
-  client_id    = each.value == "DTS-HERITAGE-EXTSVC-PROD" ? "5f29910d-50d4-424b-a888-e95c59dc9d70" : null
+  client_id    = each.value == "DTS-HERITAGE-EXTSVC-PROD" ? "8599b398-aa85-4f3e-ade6-47fb2e70176c" : null
 }
