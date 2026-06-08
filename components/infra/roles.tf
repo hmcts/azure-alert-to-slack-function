bbccaa1e4b4d9c1c882b287a5370bba62765f4be
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-06-08 12:35:25 +0100

    limit roles to only where needed

--- components/infra/roles.tf
+++ components/infra/roles.tf
@@ -7,7 +7,7 @@ resource "azurerm_role_assignment" "monitoring_reader" {
 }
 
 resource "azurerm_role_assignment" "user_access_administrator" {
-  for_each             = local.business_area == "cft" ? toset(local.cft_subscriptions) : toset(local.sds_subscriptions)
+  for_each             = local.business_area == "cft" ? toset(local.cft_user_access_subs) : toset(local.sds_user_access_subs)
   scope                = azurerm_monitor_action_group.action_group.id
   role_definition_name = "User Access Administrator"
   principal_id         = data.azuread_service_principal.bootstrap[each.value].object_id
