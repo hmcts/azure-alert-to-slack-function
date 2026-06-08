3d994979dcb74e6235e944e61027e66906a0bfed
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-06-08 12:29:01 +0100

    change role

--- components/infra/roles.tf
+++ components/infra/roles.tf
@@ -6,10 +6,10 @@ resource "azurerm_role_assignment" "monitoring_reader" {
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "contributor" {
+resource "azurerm_role_assignment" "user_access_administrator" {
   for_each             = local.business_area == "cft" ? toset(local.cft_subscriptions) : toset(local.sds_subscriptions)
   scope                = azurerm_monitor_action_group.action_group.id
-  role_definition_name = "Contributor"
+  role_definition_name = "User Access Administrator"
   principal_id         = data.azuread_service_principal.bootstrap[each.value].object_id
   principal_type       = "ServicePrincipal"
 }
