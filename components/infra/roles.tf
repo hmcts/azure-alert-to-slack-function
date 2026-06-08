1d00a457b7d0475388a694d2cf8df3efd58bb966
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-06-08 11:04:08 +0100

    remove for each

--- components/infra/roles.tf
+++ components/infra/roles.tf
@@ -7,7 +7,6 @@ resource "azurerm_role_assignment" "monitoring_reader" {
 }
 
 resource "azurerm_role_assignment" "jenkins_monitoring_reader" {
-  for_each             = local.business_area == "cft" ? toset(local.cft_subscriptions) : toset(local.sds_subscriptions)
   scope                = azurerm_monitor_action_group.action_group.id
   role_definition_name = "Monitoring Reader"
   principal_id         = data.azurerm_user_assigned_identity.jenkins.principal_id
