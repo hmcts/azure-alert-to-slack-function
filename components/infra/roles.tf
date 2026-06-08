295ac154236668400d25f3ea7fad4f26be0bf53c
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-06-08 09:40:38 +0100

    add new role for jenkins

--- components/infra/roles.tf
+++ components/infra/roles.tf
@@ -5,3 +5,11 @@ resource "azurerm_role_assignment" "monitoring_reader" {
   principal_id         = data.azuread_service_principal.bootstrap[each.value].object_id
   principal_type       = "ServicePrincipal"
 }
+
+resource "azurerm_role_assignment" "jenkins_monitoring_reader" {
+  for_each             = local.business_area == "cft" ? toset(local.cft_subscriptions) : toset(local.sds_subscriptions)
+  scope                = azurerm_monitor_action_group.action_group.id
+  role_definition_name = "Monitoring Reader"
+  principal_id         = data.azurerm_user_assigned_identity.jenkins.principal_id
+  principal_type       = "ServicePrincipal"
+}
